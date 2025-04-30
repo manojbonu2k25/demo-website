2e76502897336123577f7c537fc47bd938491938
manoj-40119 <manojkumarbonu1999@gmail.com>
2025-04-30 15:46:01 -0700

    calculator

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 console.log("Site is running!");
+function appendValue(val) {
+    document.getElementById("display").value += val;
+  }
+  
+  function clearDisplay() {
+    document.getElementById("display").value = "";
+  }
+  
+  function calculate() {
+    try {
+      const result = eval(document.getElementById("display").value);
+      document.getElementById("display").value = result;
+    } catch (e) {
+      alert("Invalid Expression");
+    }
+  }
+  
